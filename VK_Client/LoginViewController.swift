6ffb78fccc879c34b8a204b8b1557d8a364625b5
Rayen <zufirka@icloud.com>
2020-11-12 22:32:48 +0300

    Добавить в приложение ​UITabbarViewController,​ три ​UITableViewController и один UICollectionViewController.​ После того как пользователь ввел верные логин и пароль, UITabbarViewController.​ Добавить две вкладки в ​UITabbarViewController.​ На первой вкладке настроить переходы в следующем порядке: ​UINavigationController — UITableViewController — UICollectionViewController. ​Это будущая вкладка для отображения друзей пользователя «ВКонтакте​» и его фотографий. Переход с таблицы на коллекцию должен происходить по нажатию на ячейку. На второй вкладке — в таком порядке: ​UINavigationController — UITableViewController — UITableViewController.​Первый контроллер для отображения групп пользователя, второй — для отображения глобального поиска групп, которые могут быть интересны пользователю. Для перехода с первой таблицы на вторую на ​NavigationBar необходимо создать ​Bar Button Item.​

--- VK_Client/LoginViewController.swift
+++ VK_Client/LoginViewController.swift
@@ -11,49 +11,86 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-// Кнопка изначально не активная.
+        
+        // Кнопка изначально не активная.
         signInButton.isEnabled = false
-
-// Наблюдатель за изменением введенных значений в Text Field.
+        
+        // Наблюдатель за изменением введенных значений в Text Field.
         loginTextField.addTarget(self, action: #selector(textFieldDidChange), for: UIControl.Event.editingChanged)
         passwordTextField.addTarget(self, action: #selector(textFieldDidChange), for: UIControl.Event.editingChanged)
-
+        
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         scrollView.addGestureRecognizer(hideKeyboardGesture)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(keyboardWasShown(notification:)),
                                                name: UIResponder.keyboardWillShowNotification,
                                                object: nil)
-
+        
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(keyboardWillBeHidden(notification:)),
                                                name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        let checkResult = checkUserData()
+        
+        if !checkResult {
+            showLoginError()
+        }
+        
+        return checkResult
+    }
+    
+    func checkUserData() -> Bool {
+        
+        let login = loginTextField.text
+        let password = passwordTextField.text
+        
+        if login == "Rayen" && password == "root" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func showLoginError() {
+        let alert = UIAlertController(title: "Error",
+                                      message: """
+                                                   Your Login: Rayen
+                                                   Your Password: root
+                                                   """,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok",
+                                   style: .cancel)
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
     @IBAction func pressedSignInButton(_ sender: UIButton) {
     }
 }
 
 extension LoginViewController {
-
-// Если поля Text Field пустые, то кнопка будет не активна.
+    
+    // Если поля Text Field пустые, то кнопка будет не активна.
     @objc func textFieldDidChange(textField: UITextField) {
         if loginTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
             signInButton.isEnabled = false
@@ -61,24 +98,24 @@ extension LoginViewController {
             signInButton.isEnabled = true
         }
     }
-
+    
     @objc func keyboardWasShown(notification: Notification) {
-
+        
         let info = notification.userInfo! as NSDictionary
         let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
         let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.height, right: 0)
-
+        
         self.scrollView.contentInset = contentInsets
         scrollView.scrollIndicatorInsets = contentInsets
     }
-
+    
     @objc func keyboardWillBeHidden(notification: Notification) {
-
+        
         let contentInsets = UIEdgeInsets.zero
         scrollView.contentInset = contentInsets
     }
-
-
+    
+    
     @objc func hideKeyboard() {
         self.scrollView.endEditing(true)
     }
